457d1f2d645ba08458345f9d3d8480afc697f747
Jason Howe <jason.howe223@gmail.com>
2023-09-29 19:49:05 -0400

    add better CLI parsing and file validation

--- code/sparrow.py
+++ code/sparrow.py
@@ -3,9 +3,13 @@
 # A Functional HTTP Server, written in python
 
 import sys
+import os
+
 import request
 import response
 import logger
+import config
+
 
 def parser_handler(req: request.Request) -> response.response:
 
@@ -51,41 +55,75 @@ def parser_handler(req: request.Request) -> response.response:
         logger.log("BAD", req, res)
         return
 
+def check_file(filepath) -> bool:
+    # ensure file exists
+    if not os.path.exists(filepath):
+        return False
+    # ensure file is readable
+    if not os.access(filepath, os.R_OK):
+        return False
+    # things are good
+    return True
+
+
+def parse_cli(args: list) -> list:
+    if len(args) == 2:
+        print("[+] Entering File Mode")
+        config.MODE = config.Server_Mode.FILE
+    elif len(args) == 3:
+        print("[+] Entering Server Mode Using HTTP")
+        config.MODE = config.Server_Mode.HTTP
+    elif len(args) == 5:
+        print("[+] Entering Server Mode Using HTTPS")
+        config.MODE = config.Server_Mode.HTTPS
+    else:
+        usage()
+        
+    if config.MODE == config.Server_Mode.FILE:
+        if not check_file(args[1]):
+            print("[-] Error Reading HTTP Request File. Check file path and file permissions.\n"\
+                  "[-] Exiting Sparrow\n")
+            sys.exit(1)
+        return
+
+    elif config.MODE == config.Server_Mode.HTTP:
+        # do HTTP stuff here
+        return
+
+    elif config.MODE == config.Server_Mode.HTTPS:
+        # do HTTPS stuff here
+        if not check_file(args[3]):
+            print("[-] Error Reading HTTPS Public Key File. Check file path and file permissions.\n"\
+                  "[-] Exiting Sparrow\n")
+            sys.exit(1)
+        elif not check_file(args[4]):
+            print("[-] Error Reading HTTPS Public Key File. Check file path and file permissions.\n"\
+                  "[-] Exiting Sparrow\n")
+            sys.exit(1)
+        return
+
 
 def usage():
-    print("Sparrow Web Server\n"\
-          "To use file mode: python3 sparrow.py <path/to/file>\n"\
-          "To use HTTP Server mode: python3 sparrow.py <ip> <port>\n"
-          "To use HTTPS Server mode: python3 sparrow.py <ip> <port> <pub key> <priv key>"
+    print("[-] CLI Argument Error.\n"\
+          "[-] To use file mode: python3 sparrow.py <path/to/file>\n"\
+          "[-] To use HTTP Server mode: python3 sparrow.py <ip> <port>\n"\
+          "[-] To use HTTPS Server mode: python3 sparrow.py <ip> <port> <pub key> <priv key>\n"\
+          "[+] Exiting Sparrow\n"
           )
+    sys.exit(1)
+
 
 
 if __name__ == "__main__":
-    # check sys arg length to get mode
-    if len(sys.argv) == 2:
-        print("file mode")
-        sys.exit(0)
-    if len(sys.argv) == 3:
-        print("http mode")
-        sys.exit(0)
-    elif len(sys.argv) == 5:
-        print("https mode")
-        sys.exit(0)
-    else:
-        usage()
-        sys.exit(0)
+    # parse/validate CLI input
+    print("[+] Starting Sparrow Web Server")
+    print("[+] Parsing CLI Arguements")
+    args = parse_cli(sys.argv)
+    print("[+] CLI Arguements Parsed")
+    print("[+] Initializing Logger")
+    logger.init_logger()
+    print("[+] Logger Initialized")
 
-
-    request_file = sys.argv[1]
-    with open(request_file, "r") as rfile:
-        req_str = rfile.read()
     
-
-    err, req = request.parse(req_str)
-    if err == 1:
-        res = response.return_400()
-        logger.log("BAD", req, res)
-        sys.exit(0)
-    
-    parser_handler(req)
+    # parser_handler(req)
     
