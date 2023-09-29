6224b65959ec63184ba04cfe765cfd1414702256
Jason Howe <jason.howe223@gmail.com>
2023-09-29 15:49:49 -0400

    add parser_handler() function

--- code/sparrow.py
+++ code/sparrow.py
@@ -6,7 +6,49 @@ import sys
 import request
 import response
 
+def parser_handler(req: request.Request) -> response.response:
 
+    ver_check = request.validate_version(req.version)
+    if ver_check != True:
+        res = response.return_400()
+        print(res) # TODO: switch to log
+        return
+
+    page_check = request.validate_page(req.page)
+    if page_check != True:
+        res = response.return_400()
+        print(res) # TODO: switch to log
+        return
+
+    method_check = request.validate_method(req.method)
+    if method_check != True:
+        res = response.return_400()
+        print(res) # TODO: switch to log
+        return
+
+    if req.method == "GET":
+        err = request.parse_get(req)
+    elif req.method == "POST":
+        err = request.parse_post(req)
+    elif req.method == "PUT":
+        err = request.parse_put(req)
+    elif req.method == "DELETE":
+        err = request.parse_delete(req)
+    elif req.method == "HEAD":
+        err = request.parse_head(req)
+
+    if err == 0:
+        res = response.return_200()
+        print(res) # TODO: switch to log
+        return
+    elif err == 1:
+        res = response.return_400()
+        print(res) # TODO: switch to log
+        return
+    elif err == 2:
+        res = response.return_500()
+        print(res) # TODO: switch to log
+        return
 
 
 if __name__ == "__main__":
@@ -18,31 +60,8 @@ if __name__ == "__main__":
     err, req = request.parse(req_str)
     if err == 1:
         res = response.return_400()
-        print(res)
+        print(res) # TODO: switch to log
+        sys.exit(0)
     
-    if req.method == "GET":
-        err = request.parse_get(req)
-    elif req.method == "POST":
-        err = request.parse_post(req)
-    elif req.method == "PUT":
-        err = request.parse_put(req)
-    elif req.method == "DELETE":
-        err = request.parse_delete(req)
-    elif req.method == "HEAD":
-        err = request.parse_head(req)
-    else:
-        err = 1
-    
-
-    if err == 0:
-        res = response.return_200()
-        print(res)
-    elif err == 1:
-        res = response.return_400()
-        print(res)
-    elif err == 2:
-        res = response.return_500()
-        print(res)
-
-
+    parser_handler(req)
     
