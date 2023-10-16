6ae460be2ca027fd00bce5c668bb70da05335385
Jason Howe <jason.howe223@gmail.com>
2023-10-16 18:16:30 -0400

    add basic header lists and header checking

--- code/headers.py
+++ code/headers.py
@@ -2,38 +2,88 @@
 # Author: Jason Howe
 # Handles Request and Response Headers
 
-# TODO: create method for validating general headers, entity headers, etc
+GENERAL_HEADERS = [
+    "Cache-Control",
+    "Connection",
+    "Date",
+    "Pragma",
+    "Trailer",
+    "Trasnfer-Encoding",
+    "Upgrade",
+    "Via",
+    "Warning"
+]
+
+REQUEST_HEADERS = [
+    "Accept",
+    "Accept-Charset",
+    "Accept-Encoding",
+    "Accept-Language",
+    "Authorization",
+    "Expect",
+    "From",
+    "Host",
+    "If-Match",
+    "If-Modified-Since",
+    "If-None-Match",
+    "If-Range",
+    "If-Unmodified-Since",
+    "Max-Forwards",
+    "Pragma",
+    "Proxy-Authorization",
+    "Range",
+    "Referer",
+    "TE" ,
+    "User-Agent",
+]
+
+RESPONSE_HEADERS = [
+    "Accept-Ranges",
+    "Age",
+    "ETag",
+    "Location",
+    "Proxy-Atuenticate",
+    "Retry-After",
+    "Server",
+    "Vary",
+    "WWW-Authenticate"
+]
+
+ENTITY_HEADERS = [
+    "Allow",
+    "Content-Encoding",
+    "Content-Language",
+    "Content-Length",
+    "Content-Location",
+    "Content-MD5",
+    "Content-Range",
+    "Expires",
+    "Last-Modified",
+
+]
+
+
 def validate_request_headers(headers: dict) -> bool:
-    valid_headers = ["Accept",
-                    "Accept-Charset",
-                    "Accept-Encoding",
-                    "Accept-Language",
-                    "Authorization",
-                    "Cache-Control",
-                    "Connection",
-                    "Date",
-                    "Expect",
-                    "From",
-                    "Host",
-                    "If-Match",
-                    "If-Modified-Since",
-                    "If-None-Match",
-                    "If-Range",
-                    "If-Unmodified-Since",
-                    "Max-Forwards",
-                    "Pragma",
-                    "Proxy-Authorization",
-                    "Range",
-                    "Referer",
-                    "TE" ,
-                    "Trailer",
-                    "Transfer-Encoding",
-                    "Upgrade",
-                    "User-Agent",
-                    "Via",
-                    "Warning"
-    ]
-    for key in headers:
-        if key not in valid_headers:
-            return False
-    return True
+    
+
+    # header dicts
+    general_headers = {}
+    request_headers = {}
+    entity_headers = {}
+    discard_headers = {}
+
+    # run thru all of the headers. lump headers into different dicts
+    for hname in headers:
+        if hname in GENERAL_HEADERS:
+            general_headers[hname] = headers[hname]
+        elif hname in REQUEST_HEADERS:
+            request_headers[hname] = headers[hname]
+        elif hname in ENTITY_HEADERS:
+            entity_headers[hname] = headers[hname]
+        else:
+            discard_headers[hname] = headers[hname]
+
+
+
+
+    
